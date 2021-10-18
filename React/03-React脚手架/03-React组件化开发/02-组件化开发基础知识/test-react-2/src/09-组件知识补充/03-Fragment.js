0e3324cd09675cb5d3506d1667307a1b4430edec
astfn <876458652@qq.com>
2021-10-18 13:22:45 +0800

    update React documents

---
+++ React/03-React脚手架/03-React组件化开发/02-组件化开发基础知识/test-react-2/src/09-组件知识补充/03-Fragment.js
@@ -0,0 +1,13 @@
+import { Fragment, PureComponent } from "react";
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <Fragment>
+        <h2>Ashun</h2>
+        <h2>Ashuntefannao</h2>
+        <h2>ASHUN</h2>
+      </Fragment>
+    );
+  }
+}
