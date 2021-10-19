68b51bb01d0b2dc07fc630286e2bca3bc7581cf2
astfn <876458652@qq.com>
2021-10-19 10:03:37 +0800

    update React doucments

---
+++ React/03-React脚手架/03-React组件化开发/02-组件化开发基础知识/test-react-2/src/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// import App from "./01组件的嵌套/App.js";
+
+// import App from "./02-父子组件的通讯/父传子.js";
+// import App from "./02-父子组件的通讯/TabControl实现/App.js";
+
+// import App from "./03-插槽的实现/App.js";
+
+// import App from "./04-跨组件通讯/01-跨组件通讯-props/App.js";
+// import App from "./04-跨组件通讯/02-跨组件通讯-Context/01-过程梳理/App.js";
+// import App from "./04-跨组件通讯/02-跨组件通讯-Context/02-Provider的嵌套/App.js";
+// import App from "./04-跨组件通讯/03-跨组件通讯-事件总线/App.js";
+
+// import App from "./05-setState详解/01-setState基础.js";
+// import App from "./05-setState详解/02-异步变更获取.js";
+// import App from "./05-setState详解/03-同步变更.js";
+// import App from "./05-setState详解/04-setState的合并.js";
+
+// import App from "./06-React性能优化/01-keys的优化.js";
+// import App from "./06-React性能优化/02-SCU优化/01-单组件无用更新.js";
+// import App from "./06-React性能优化/02-SCU优化/02-组件嵌套导致无用更新.js";
+// import App from "./06-React性能优化/02-SCU优化/03-PureComponent.js";
+// import App from "./06-React性能优化/02-SCU优化/04-memo组件.js";
+// import App from "./06-React性能优化/02-SCU优化/05-为什么保持state的不可变性.js";
+
+// import App from "./07-受控和非受控组件/01-refs的使用.js"
+// import App from "./07-受控和非受控组件/02-受控组件/01-受控组件的基本使用.js";
+// import App from "./07-受控和非受控组件/02-受控组件/02-处理多个输入项.js";
+// import App from "./07-受控和非受控组件/02-受控组件/03-其他表单元素/01-select标签.js";
+// import App from "./07-受控和非受控组件/02-受控组件/03-其他表单元素/02-checkbox按钮.js";
+// import App from "./07-受控和非受控组件/03-非受控组件/01-非受控组件的使用.js";
+
+// import App from "./08-高阶组件/01-高阶组件的定义/App.js";
+// import App from "./08-高阶组件/02-高阶组件的应用/01-props的扩充.js";
+// import App from "./08-高阶组件/02-高阶组件的应用/02-Context共享props.js";
+// import App from "./08-高阶组件/02-高阶组件的应用/03-渲染判断鉴权.js";
+// import App from "./08-高阶组件/02-高阶组件的应用/04-生命周期的拦截.js";
+
+// import App from "./09-组件知识补充/01-Ref的转发forwardRef.js";
+
+// import App from "./09-组件知识补充/02-createPortal.js";
+// import "./09-组件知识补充/02-createPortal.css";
+
+// import App from "./09-组件知识补充/03-Fragment.js";
+import App from "./09-组件知识补充/04-StrictMode.js";
+ReactDOM.render(<App />, document.querySelector("#root"));
